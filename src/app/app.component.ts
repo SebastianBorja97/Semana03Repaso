7bc8e460b08ffe0de20b80ce56536bb4073594bc
sborja <sborjabustamante@gmail.com>
2021-09-24 13:06:11 -0500

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Semana03Repaso';
+  title = 'Semana03Repaso - Sebastian Borja';
 }
